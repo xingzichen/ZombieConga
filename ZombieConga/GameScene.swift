76620585fc25220361d4f20f8a50bf66ee8874ab
Liang.Michael <xzc2116@gmail.com>
2014-06-21 13:57:53 +0800

    make zombie moving by touching screen

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,11 +11,14 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    let ZOMBIE_MOVE_POINTS_PER_SEC:CFloat = 120.0
+    let ZOMBIE_MOVE_POINTS_PER_SEC:CGFloat = 120.0;
+    let ZOMBIE_ROTATE_RADIANS_PER_SEC = 2*M_PI;
     
     var _lastUpdateTime:NSTimeInterval = 0.0;
     var _dt:NSTimeInterval = 0.0;
     var _velocity:CGPoint = CGPointZero;
+    var _zombie:SKSpriteNode = SKSpriteNode(imageNamed:"zombie1");
+    var _zombieTowardLocation = CGPointZero;
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -25,32 +28,16 @@ class GameScene: SKScene {
         let bg = SKSpriteNode(imageNamed:"background");
         bg.position = CGPoint(x: view.bounds.size.width/2,y: view.bounds.size.height/2);
         bg.anchorPoint = CGPoint(x: 0.5,y: 0.5);
-//        bg.anchorPoint = CGPointZero;
         self.addChild(bg);
+        
+        _zombie.position = CGPointMake(100,100);
+        self.addChild(_zombie);
+        
+        _zombieTowardLocation = _zombie.position;
         
         println("View bounds \(view.bounds)");
         println("Scene size \(self.size)")
 
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
@@ -62,14 +49,85 @@ class GameScene: SKScene {
         }
         
         _lastUpdateTime = currentTime;
-        println("\(_dt)");
+        
+        if( CGPointLength(const: CGPointSubtract(_zombie.position, b: _zombieTowardLocation)) >= CGPointLength(const:CGPointMultiplyScalar(_velocity, b: _dt))){
+            self.moveSprite(_zombie, velocity:_velocity);
+            self.rotateSprite(_zombie, direction:_velocity, rotateRadiansPerSec: ZOMBIE_ROTATE_RADIANS_PER_SEC);
+        }
+        else {
+            _zombie.position = _zombieTowardLocation;
+        }
+        
+//        println("\(_dt)");
     }
+    
+    
+    
+    
+    // MARK: - move
     
     func moveSprite(sprite:SKSpriteNode, velocity:CGPoint){
         var amountToMove:CGPoint = CGPointMultiplyScalar(velocity, b: _dt);
+        sprite.position = CGPointAdd(sprite.position, b:amountToMove);
     }
     
-    // MARK: - helper methods
+    func moveZombieToward(location:CGPoint){
+        _zombieTowardLocation = location;
+        var offset = CGPointSubtract(location, b: _zombie.position);
+        var direction = CGPointNormalize(const: offset);
+        _velocity = CGPointMultiplyScalar(direction, b: ZOMBIE_MOVE_POINTS_PER_SEC);
+        println("zombie position : \(_zombie.position) , touch point : \(_zombieTowardLocation)");
+    }
+    
+    func rotateSprite(sprite:SKSpriteNode, direction:CGPoint){
+        sprite.zRotation = CGPointToAngle(const: direction);
+    }
+    
+    func rotateSprite(sprite:SKSpriteNode, direction:CGPoint, rotateRadiansPerSec:CGFloat){
+        var rotation = ScalarShortestAngleBetween(const: CGPointToAngle(const: direction), const: sprite.zRotation);
+        println("rotation : \(rotation)")
+        var amtToRotate = rotateRadiansPerSec*_dt;
+        if (rotation>0) {
+            if(rotation - amtToRotate < 0){
+                sprite.zRotation = CGPointToAngle(const: direction);
+            }else{
+                sprite.zRotation -= amtToRotate;
+            }
+        }else{
+            if(rotation + amtToRotate > 0){
+                sprite.zRotation = CGPointToAngle(const: direction);
+            }else{
+                sprite.zRotation += amtToRotate;
+            }
+        }
+    }
+    
+    
+    // MARK: - Touch Actions
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        let touch = touches.anyObject() as UITouch;
+        var touchLocation = touch.locationInNode(self);
+        self.moveZombieToward(touchLocation);
+    }
+
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        let touch = touches.anyObject() as UITouch;
+        var touchLocation = touch.locationInNode(self);
+        self.moveZombieToward(touchLocation);
+
+    }
+    
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+        let touch = touches.anyObject() as UITouch;
+        var touchLocation = touch.locationInNode(self);
+        self.moveZombieToward(touchLocation);
+
+    }
+    
+    // MARK: - Helper Methods
     
     func CGPointMultiplyScalar(a:CGPoint,b:CGFloat)->CGPoint{
         return CGPointMake(a.x*b, a.y*b);
